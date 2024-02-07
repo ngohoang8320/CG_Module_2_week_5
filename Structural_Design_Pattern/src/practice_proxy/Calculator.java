60384752721cbb68b67d01d8425d87d1364f4bed
Ngo Hoang <ngohoang8320@gmail.com>
2024-02-07 17:43:19 +0700

    Add project Structural_Design_Pattern with the pratice package about Proxy DP.

---
+++ Structural_Design_Pattern/src/practice_proxy/Calculator.java
@@ -0,0 +1,15 @@
+package practice_proxy;
+
+public interface Calculator {
+    double add(double first,
+               double second);
+
+    double sub(double first,
+               double second);
+
+    double mul(double first,
+               double second);
+
+    double div(double first,
+               double second);
+}
