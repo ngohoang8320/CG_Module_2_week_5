b5b0f9f1ebc328b3d4f527627d42050783086fe1
Ngo Hoang <ngohoang8320@gmail.com>
2024-02-07 21:45:59 +0700

    Add project Structural_Design_Pattern with the pratice package about Adapter DP.

---
+++ Structural_Design_Pattern/src/practice_adapter/Client.java
@@ -0,0 +1,25 @@
+package practice_adapter;
+
+import java.util.Scanner;
+
+public class Client {
+    private FileCalculator fileCalculator;
+
+    public Client(FileCalculator fileCalculator) {
+        this.fileCalculator = fileCalculator;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file or folder path: ");
+        String path = scanner.nextLine();
+        FileCalculator fileCalculator = new FileCalculatorAdapter();
+        Client client = new Client(fileCalculator);
+        client.printFileSize(path);
+    }
+
+    public void printFileSize(String path) {
+        long size = fileCalculator.calculateSize(path);
+        System.out.println("Size: " + size);
+    }
+}
---
+++ Structural_Design_Pattern/src/practice_adapter/FileCalculator.java
@@ -0,0 +1,5 @@
+package practice_adapter;
+
+public interface FileCalculator {
+    long calculateSize(String path);
+}
