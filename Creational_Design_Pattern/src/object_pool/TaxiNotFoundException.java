120f818b62e6aaccf3d3916423001ea194719f3e
Ngo Hoang <ngohoang8320@gmail.com>
2024-02-07 11:18:50 +0700

    Add project Creational_Design_Pattern with the pratice package about Object Pool.

---
+++ Creational_Design_Pattern/src/object_pool/TaxiNotFoundException.java
@@ -0,0 +1,7 @@
+package object_pool;
+
+public class TaxiNotFoundException extends RuntimeException {
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
+}
